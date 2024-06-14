855a47631332fdf361a4b3ef4289b6270ef97970
Mark Gibson <markus.gibson@gmail.com>
2024-06-14 19:27:18 -0400

    Initial commit

---
+++ test/datadog-vpcendpoint-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DatadogVpcendpointCdk from '../lib/datadog-vpcendpoint-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/datadog-vpcendpoint-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DatadogVpcendpointCdk.DatadogVpcendpointCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
